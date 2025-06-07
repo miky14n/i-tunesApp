20981bcff09e193b1b4375a3dcb1b0bdbea149d0
miky14n <miky14n@gmail.com>
2025-06-07 14:08:22 -0400

    Change name of first component

---
+++ src/components/InputText/index.jsx
@@ -0,0 +1,41 @@
+
+export default function InputText({
+  filterOptions,
+  title = "",
+  personalPlaceHolder = "put any text here",
+  onInputChange = () => {},
+  onSelectChange = () => {}
+}) {
+  return (
+    <div>
+      <label htmlFor="search" className="block text-sm font-medium text-gray-900">
+        {title}
+      </label>
+      <div className="mt-2">
+        <div className="flex items-center rounded-md bg-white pl-3 outline outline-1 outline-gray-300 focus-within:outline-2 focus-within:outline-indigo-600">
+          <input
+            id="search"
+            name="search"
+            type="text"
+            placeholder={personalPlaceHolder}
+            onChange={(e) => onInputChange(e.target.value)}
+            className="block flex-1 py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none"
+          />
+          <select
+            id="filter"
+            name="filter"
+            aria-label="Filter"
+            onChange={(e) => onSelectChange(e.target.value)}
+            className="ml-2 py-1.5 pr-7 pl-3 text-base text-gray-500 rounded-md focus:outline-indigo-600"
+          >
+            {filterOptions.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}
