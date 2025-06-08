1650db0e36a056efcfa079f187ac2e7eb5d664cc
miky14n <miky14n@gmail.com>
2025-06-07 20:33:22 -0400

    Fix chose only 1 album of collection data

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,12 +1,30 @@
 
 const API_URL = "https://itunes.apple.com/search?term=";
 
+export function sortByAlbun(data) {
+  try {
+    const seen = {}; 
+    const uniqueByCollectionId = [];
+
+    for (const item of data) {
+      if (!seen[item.collectionId]) {
+        uniqueByCollectionId.push(item);
+        seen[item.collectionId] = true; 
+      }
+    }
+    return uniqueByCollectionId;
+  } catch (error) {
+    console.error("Error en getDataFilter:", error.message);
+    return [];
+  }
+}
+
 export async function fetchItunesData(search) {
     try {
         const response = await fetch(API_URL + search);
         const result = await response.json();
-        console.log(result, "La rp de api");
-        return result["results"];
+        const data=sortByAlbun(result["results"])
+        return data;
     } catch (error) {
         console.error("Error al obtener datos:", error);
         return [];
